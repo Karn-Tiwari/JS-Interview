e295f9d98d97bb53952af76aac0285edf83bbeb1
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2024-02-28 11:11:34 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "234vdfn"// so for this case we have to think wisely for handling the number value suppose which is coming from any form
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);//number
+console.log(valueInNumber);// NaN
+
+
+//1=>true; 0=>false
+// "" =>false
+//"karn" =>true
