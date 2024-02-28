c2320f090cd1391ffe8295f114b731ce4dc84a8f
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2024-02-28 11:01:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"// Treat all JS code as newer version
+
+// alert(3+3) We are using nodejs, not browser
+
+ let name = "Karn"
+ let age = 19
+ let isMarried = false
+
+ // null //But type of null is object //=>standalone value // example->accessing value of temperature from browser but due to some error it will send null value 
+
+ //undefined
+
+ // symbol =>unique 
