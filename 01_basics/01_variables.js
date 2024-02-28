a286f57eff9504a431d5bae9b940d94d697df75c
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2024-02-28 11:01:40 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accoundId = 23535
+let accountEmail = "karn@gmail.com"
+var accountpassword = "23235"
+accountCity = "Jaipur"
+
+// accoundId = 2; not allowed
+console.log(accoundId);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
