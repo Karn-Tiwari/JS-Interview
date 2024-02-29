52cf871f6c21e7aaafbdd13ffc09fd3c4b2e652e
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2024-02-29 08:52:36 +0000

    string play

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,21 +11,79 @@ array, objects,functions
 
 //Question JS is statically type or dynamically type
 
-let userEmail;
+// let userEmail;
 
-const id = Symbol("123")// it gives unique value always in symbol type
+// const id = Symbol("123")// it gives unique value always in symbol type
 
-const anotherId = Symbol("123")
+// const anotherId = Symbol("123")
 
-console.log(id===anotherId);
+// console.log(id===anotherId);
 
-const heroes = ["shaktiman","naagraj","dogra"]
+// const heroes = ["shaktiman","naagraj","dogra"]
 
-let myObj={
-    name:"karn",
-    age:22,
-}
+// let myObj={
+//     name:"karn",
+//     age:22,
+// }
 
-const myFuntion = function(){
-    console.log("Karn Tiwari")
-}
+// const myFuntion = function(){
+//     console.log("Karn Tiwari")
+// }
+
+
+
+//*******************************************
+// stack (Primitive), Heap (Non, Primitive ) 
+
+// let myYoutubeName = "Karn Tiwari"
+
+// let anotherName = myYoutubeName
+// anotherName = "Shivam Tiwari"
+// console.log(myYoutubeName)//Karn Tiwari
+// console.log(anotherName)// Shivam Tiwari
+
+
+//But in case of Non Primitive it reference to the same point so any change cause change in refernce also
+
+// let myObj = {
+//     email:"karn@gmail.com",
+//     upi:"kt@123"
+// }
+
+// let yourObj = myObj
+// yourObj.email = "Shivam@gmail.com"
+
+// console.log(myObj.email)//Shivam@gmail.com
+// console.log(yourObj.email)//Shivam@gmail.com
+
+// const name = "karn"
+// const repoCount = 50
+// // console.log(name + repCount + "value") this is a old ways of writing string in concatinaation
+
+// console.log(`my name is ${name} and number of repositary is ${repoCount}`);
+
+const gameName = new String("Karn Tiwari")
+
+console.log(gameName[0])
+
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(5))
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0,4)//it start from 0 index and not include 4th index
+console.log(newString)
+
+const anotherString = gameName.slice(-2,4)
+console.log(anotherString)//it gives the character from end
+const newStringOne = "    Karn      "
+console.log(newStringOne)
+console.log(newStringOne.trim())//It trim the space
+
+const url = "https://karn.com//karn%20tiwari"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('Shivam')) 
