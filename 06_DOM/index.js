b9773fe82c675fd3c2a378bac89813957f34ed7d
Karn Tiwari <86906783+Karn-Tiwari@users.noreply.github.com>
2024-03-09 18:48:30 +0000

    DOM mainpulation

---
+++ 06_DOM/index.js
@@ -0,0 +1,4 @@
+// difference between innerHTML, innerText, and textContent
+// innerHTML: returns the HTML content of an element isme html tags bhi dikh jayenge
+// innerText: returns the visible text of the element
+// textContent: returns all the text content of an element wo bhi element dikh jayega jo kisi css proprty se chhupa hua hoga
